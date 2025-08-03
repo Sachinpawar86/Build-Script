c9042dd07c415c868203997a2035b21728938bf3
Sachinpawar86 <106669438+Sachinpawar86@users.noreply.github.com>
2025-08-03 21:44:21 +0530

    Add files via upload

---
+++ evox.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
+
+# Rom source repo
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b Evo-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+echo "============================"
